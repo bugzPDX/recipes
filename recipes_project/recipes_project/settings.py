38acb385bb1c1d61c888f268cdb2bb1a8021d98a
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2014-03-01 15:21:15 -0800

    Edited files in settings.py to fix error on Heroku

--- recipes_project/recipes_project/settings.py
+++ recipes_project/recipes_project/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes',
     'registration',
+    'recipes',
     'gunicorn',
 )
 
