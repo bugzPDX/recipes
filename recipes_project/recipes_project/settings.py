94b20468b194a1cc7a1a6691e7ab4d782f038e54
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2014-03-08 14:10:18 -0800

    Added the ability to generate admin documentation

--- recipes_project/recipes_project/settings.py
+++ recipes_project/recipes_project/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django.contrib.admindocs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
